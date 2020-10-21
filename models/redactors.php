a2b7a1a9cab875608df3e12db5f9684e06aabb40
OxyTom <oxytom8@gmail.com>
2020-10-21 21:29:05 +0200

    api finished ?

--- models/redactors.php
+++ models/redactors.php
@@ -7,21 +7,56 @@ header('Content-Type: application/json');
 class RedactorList
 {
     public $redactors;
+    public $sucess = true;
 }
 
 $raw = new RedactorList;
 
+if (!isset($_POST['method']) or $_POST['method'] == 'GET') {
+    $id = $_GET['ID'];
+    if ($id == '')
+        $result = $objPdo->prepare("SELECT id_redactor, last_name, first_name, mail FROM redactor ORDER BY id_redactor");
+    else {
+        $result = $objPdo->prepare("SELECT id_redactor, last_name, first_name, mail FROM redactor WHERE id_redactor = :id");
+        $result->bindValue('id', $id, PDO::PARAM_INT);
+    }
+} else {
+    if ($_POST['method'] == 'NEW') {
+        if (isset($_GET['lname']) and isset($_GET['fname']) and isset($_GET['mail']) and isset($_GET['pass'])) {
+            $insert_stmt = $objPdo->prepare("INSERT INTO `redactor`(`last_name`, `first_name`, `mail`, `passwrd`) VALUES (:lname, :fname, :mail, :pass)");
+            $insert_stmt->bindValue('lname', htmlentities($_POST['lname']), PDO::PARAM_STR);
+            $insert_stmt->bindValue('fname', htmlentities($_POST['fname']), PDO::PARAM_STR);
+            $insert_stmt->bindValue('mail', htmlentities($_POST['mail']), PDO::PARAM_STR);
+            $insert_stmt->bindValue('pass', crypt(htmlentities($_POST['pass']), '$2a$07$usesomesillystringforsalt'), PDO::PARAM_STR);
+        } else
+            $raw->sucess = "missing arguments";
+    } else  if ($_POST['method'] == 'UPDATE') {
+        if (isset($_GET['id']) and isset($_GET['lname']) and isset($_GET['fname']) and isset($_GET['mail']) and isset($_GET['pass'])) {
+            $insert_stmt = $objPdo->prepare("UPDATE `redactor` SET `last_name`=:lname, `first_name`=:fname, `mail`=:mail, `passwrd`=:pass WHERE `id_redactor`=:id");
+            $insert_stmt->bindValue('id', htmlentities($_GET['id']), PDO::PARAM_INT);
+            $insert_stmt->bindValue('lname', htmlentities($_POST['lname']), PDO::PARAM_STR);
+            $insert_stmt->bindValue('fname', htmlentities($_POST['fname']), PDO::PARAM_STR);
+            $insert_stmt->bindValue('mail', htmlentities($_POST['mail']), PDO::PARAM_STR);
+            $insert_stmt->bindValue('pass', crypt(htmlentities($_POST['pass']), '$2a$07$usesomesillystringforsalt'), PDO::PARAM_STR);
+        } else
+            $raw->sucess = "missing arguments";
+    } else  if ($_POST['method'] == 'DELETE') {
+        if (isset($_GET['id'])) {
+            $insert_stmt = $objPdo->prepare("DELETE FROM `redactor` WHERE `id_redactor`=:id");
+            $insert_stmt->bindValue('id', htmlentities($_GET['id']), PDO::PARAM_INT);
+        } else
+            $raw->sucess = "missing arguments";
+    } else {
+        $raw->sucess = "no method found";
+    }
+}
 
-$id = $_GET['ID'];
-if ($id == '')
-    $result = $objPdo->prepare("SELECT id_redactor, last_name, first_name, mail FROM redactor ORDER BY id_redactor");
-else {
-    $result = $objPdo->prepare("SELECT id_redactor, last_name, first_name, mail FROM redactor WHERE id_redactor = :id");
-    $result->bindValue('id', $id, PDO::PARAM_INT);
-}
-$result->execute();
-foreach ($result as $row) {
-    $raw->redactors[] = new Redactor($row['id_redactor'], $row['last_name'], $row['first_name'], $row['mail']);
-}
+if (!$result->execute()) {
+    if ($raw->sucess == true)
+        $raw->sucess = "MySQL error";
+} else
+    foreach ($result as $row) {
+        $raw->redactors[] = new Redactor($row['id_redactor'], $row['last_name'], $row['first_name'], $row['mail']);
+    }
 
 echo json_encode($raw);
--- models/themes.php
+++ models/themes.php
@@ -6,6 +6,7 @@ header('Content-Type: application/json');
 class ThemeList
 {
     public $themes;
+    public $sucess = true;
 }
 
 class Theme
@@ -25,17 +26,49 @@ class Theme
 }
 
 $raw = new ThemeList;
+if (!isset($_POST['method']) or $_POST['method'] == 'GET') {
+    $id = $_GET['ID'];
+    if ($id == '')
+        $result = $objPdo->prepare("SELECT * FROM theme ORDER BY id_theme");
+    else {
+        $result = $objPdo->prepare("SELECT * FROM theme WHERE id_theme = :id");
+        $result->bindValue('id', $id, PDO::PARAM_INT);
+    }
+} else {
+    if ($_POST['method'] == 'NEW') {
+        $result = $objPdo->prepare("INSERT INTO `theme`(`label`, `color`, `icon_theme`) VALUES (:label, :color, :iconURL)");
+        if (isset($_POST['label']) and isset($_POST['color']) and isset($_POST['iconURL'])) {
+            $result->bindValue('label', htmlentities($_POST['label']), PDO::PARAM_STR);
+            $result->bindValue('color', htmlentities($_POST['color']), PDO::PARAM_STR);
+            $result->bindValue('iconURL', htmlentities($_POST['iconURL']), PDO::PARAM_STR);
+        } else
+            $raw->sucess = "missing arguments";
+    } else  if ($_POST['method'] == 'UPDATE') {
+        $insert_stmt = $objPdo->prepare("UPDATE `theme` SET `label`=:label, `color`=:color, `icon_theme`=:iconURL WHERE `id_theme`=:id");
+        if (isset($_POST['label']) and isset($_POST['color']) and isset($_POST['iconURL']) and isset($_POST['id'])) {
+            $result->bindValue('label', htmlentities($_POST['label']), PDO::PARAM_STR);
+            $result->bindValue('color', htmlentities($_POST['color']), PDO::PARAM_STR);
+            $result->bindValue('iconURL', htmlentities($_POST['iconURL']), PDO::PARAM_STR);
+            $result->bindValue('id', htmlentities($_POST['id']), PDO::PARAM_INT);
+        } else
+            $raw->sucess = "missing arguments";
+    } else  if ($_POST['method'] == 'DELETE') {
+        $insert_stmt = $objPdo->prepare("DELETE FROM `theme` WHERE `id_theme`=:id");
+        if (isset($_POST['id'])) {
+            $result->bindValue('id', htmlentities($_POST['id']), PDO::PARAM_INT);
+        } else
+            $raw->sucess = "missing arguments";
+    } else {
+        $raw->sucess = "no method found";
+    }
+}
 
-$id = $_GET['ID'];
-if ($id == '')
-    $result = $objPdo->prepare("SELECT * FROM theme ORDER BY id_theme");
-else {
-    $result = $objPdo->prepare("SELECT * FROM theme WHERE id_theme = :id");
-    $result->bindValue('id', $id, PDO::PARAM_INT);
-}
-$result->execute();
-foreach ($result as $row) {
-    $raw->themes[] = new Theme($row['id_theme'], $row['label'], $row['color'], $row['icon_theme']);
-}
+if (!$result->execute()) {
+    if ($raw->sucess == true)
+        $raw->sucess = "MySQL error";
+} else
+    foreach ($result as $row) {
+        $raw->themes[] = new Theme($row['id_theme'], $row['label'], $row['color'], $row['icon_theme']);
+    }
 
 echo json_encode($raw);
