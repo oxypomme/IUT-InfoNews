ec4602856c40ae6b3d7bb7ca99b24a9696071d58
OxyTom <oxytom8@gmail.com>
2020-10-22 14:31:08 +0200

    fixed an error on uwampp

--- models/session.php
+++ models/session.php
@@ -11,9 +11,7 @@ if (session_id() == "")
     session_start();
 $raw->idlogin = (isset($_SESSION['login']) ?  $_SESSION['login'] : '');
 
-$result = new class
-{
-};
+$result = new stdClass();
 $name = (isset($_GET['Name']) ? $_GET['Name'] : '');
 if ($name != '' && isset($raw->$name)) {
     $result->$name = $raw->$name;
