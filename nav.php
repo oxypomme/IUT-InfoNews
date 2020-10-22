f7b37d01c1ce5303ce88a88d874a2120a305d45c
AvaN0x <clement.ricatte57@gmail.com>
2020-10-22 18:42:58 +0200

    user account is now stored in a dropdown

--- nav.php
+++ nav.php
@@ -8,19 +8,26 @@
         if (session_id() == "")
             session_start();
         if (isset($_SESSION['login'])) {
-            echo '<li class="connect"><a href="#" onclick="unlogAccount()">Déconnexion</a></li>';
-
             $id = $_SESSION['login'];
             include "models/connect.php";
             $result = $objPdo->prepare("SELECT id_redactor, last_name, first_name FROM redactor WHERE id_redactor = '$id' LIMIT 1");
             $result->execute();
             foreach ($result as $row) {
-                echo '<li class="connect"><span>' . $row['last_name'] . ' ' . $row['first_name'] . '</span></li>'; //TODO: link to profile
+                echo '<li class="dropbtn connect" onclick="showDropdown(this); updateLangDropdown();"> ' . $row['last_name'] . ' ' . $row['first_name'];
+                echo '<ul class="dropdown-content">';
+                echo '<li style="cursor: default;"><span>Profil</span></li>'; //TODO: link to profile
+                echo '<li><a href="#" onclick="unlogAccount()">Déconnexion</a></li>';
                 break;
             }
+            echo '</ul>';
+            echo '</li>';
         } else { //TODO: not a new page, but an iframe or smthg
-            echo '<li class="connect"><a href="php/login.php">Connexion</a></li>';
-            echo '<li class="connect"><a href="php/createAccount.php">Créer un compte</a></li>';
+            echo '<li class="dropbtn connect" onclick="showDropdown(this); updateLangDropdown();"> Compte';
+            echo '<ul class="dropdown-content">';
+            echo '<li><a href="php/login.php">Connexion</a></li>';
+            echo '<li><a href="php/createAccount.php">Créer un compte</a></li>';
+            echo '</ul>';
+            echo '</li>';
         }
         ?>
         <li class="dropbtn connect" onclick="showDropdown(this); updateLangDropdown();"> Langue
